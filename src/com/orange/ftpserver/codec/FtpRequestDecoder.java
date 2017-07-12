8ad8feadec8de734e1781181540022c11ac4b783
15919873635 <15919873635@126.com>
2017-07-12 14:13:15 +0800

    12345

--- src/com/orange/ftpserver/codec/FtpRequestDecoder.java
+++ src/com/orange/ftpserver/codec/FtpRequestDecoder.java
@@ -6,14 +6,23 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 public class FtpRequestDecoder extends LengthFieldBasedFrameDecoder {
 
-	private static final int MAX_PACKET_LENGTH = 8192 * 2;
+	private static final int MAX_FRAME_LENGTH = 8192 * 2;
     private static final int LENGTH_FIELD_OFFSET = 1;
     private static final int LENGTH_FIELD_LENGTH = 1;
     private static final int LENGTH_FIELD_ADJUSTMENT = 0;
     private static final int INITIAL_BYTES_TO_STRIP = 0;
 	
+    /** 
+     *  
+     * @param maxFrameLength 解码时，处理每个帧数据的最大长度 
+     * @param lengthFieldOffset 该帧数据中，存放该帧数据的长度的数据的起始位置 
+     * @param lengthFieldLength 记录该帧数据长度的字段本身的长度 
+     * @param lengthAdjustment 修改帧数据长度字段中定义的值，可以为负数 
+     * @param initialBytesToStrip 解析的时候需要跳过的字节数 
+     * @param failFast 为true，当frame长度超过maxFrameLength时立即报TooLongFrameException异常，为false，读取完整个帧再报异常 
+     */  
 	public FtpRequestDecoder() {
-		super(MAX_PACKET_LENGTH, LENGTH_FIELD_OFFSET, LENGTH_FIELD_LENGTH, LENGTH_FIELD_ADJUSTMENT, INITIAL_BYTES_TO_STRIP);
+		super(MAX_FRAME_LENGTH, LENGTH_FIELD_OFFSET, LENGTH_FIELD_LENGTH, LENGTH_FIELD_ADJUSTMENT, INITIAL_BYTES_TO_STRIP);
 	}
 
 	@Override
--- src/com/orange/ftpserver/handler/FtpServerHandler.java
+++ src/com/orange/ftpserver/handler/FtpServerHandler.java
@@ -4,6 +4,11 @@ import io.netty.channel.ChannelHandlerContext;
 
 public class FtpServerHandler extends AbstractFtpHandler {
 	
+	/**
+	 * 收到的消息没有指定的结束标记。 
+	 * 比如指定了lineBasedFrameDecoder，没有换行标志，是不会调用channelRead方法的，
+	 * 其他的类似
+	 */
 	@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)  
             throws Exception {
