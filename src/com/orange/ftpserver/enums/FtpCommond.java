9870d6e6332ce76c19902b94352e618f5be70faa
15919873635 <15919873635@126.com>
2017-07-12 11:26:54 +0800

    新增ftp命令枚举和ftp响应错误码

--- src/com/orange/ftpserver/enums/FtpCommond.java
+++ src/com/orange/ftpserver/enums/FtpCommond.java
@@ -1,10 +1,59 @@
 package com.orange.ftpserver.enums;
 
 public enum FtpCommond {
-	CWD("cws"),
-	MLD("mld"),
-	CLOSE("close"),
-	OPEN("open");
+	/** This command tells the server to abort the previous FTP service command and
+	 * any associated transfer of data. No action is to be taken if the previous
+	 * command has been completed (including data transfer). The control connection
+	 * is not to be closed by the server, but the data connection must be closed.
+	 * Current implementation does not do anything. As here data transfers are not
+	 * multi-threaded.
+	 */
+	ABOR("abor"),
+	
+	ACCT("acct"),
+	APPE("appe"),
+	AUTH("auth"),
+	CDUP("cdup"),
+	CWD("cwd"),
+	DELE("dele"),
+	EPRT("eprt"),
+	EPSV("epsv"),
+	FEAT("feat"),
+	HELP("help"),
+	LIST("list"),
+	MDTM("mdtm"),
+	MFMT("mfmt"),
+	MKD("mkd"),
+	MLSD("mlsd"),
+	MLST("mlst"),
+	MODE("mode"),
+	NLST("nlst"),
+	NOOP("noop"),
+	PASS("pass"),
+	PASV("pasv"),
+	PBSZ("pbsz"),
+	PORT("port"),
+	PWD("pwd"),
+	QUIT("quit"),
+	REIN("rein"),
+	REST("rest"),
+	RETR("retr"),
+	RMD("rmd"),
+	RNFR("rnfr"),
+	RNTO("rnto"),
+	SITE("site"),
+	SIZE("size"),
+	SITE_DESCUSER("site_descuser"),
+	SITE_HELP("site_help"),
+	SITE_STAT("site_stat"),
+	SITE_WHO("site_who"),
+	SITE_ZONE("site_zone"),
+	SATA("sata"),
+	STOR("stor"),
+	STOU("stou"),
+	STRU("stru"),
+	SYST("syst"),
+	USER("user");
 	
 	private String name;
 	
