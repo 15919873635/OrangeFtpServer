4764b6c4dab0521836b7cfb7296d15bacabe86f9
15919873635 <15919873635@126.com>
2017-07-20 09:34:34 +0800

    开发迭代

--- src/com/orange/ftpserver/exception/FtpFileException.java
+++ src/com/orange/ftpserver/exception/FtpFileException.java
@@ -1,5 +1,10 @@
 package com.orange.ftpserver.exception;
 
-public final class FtpFileException implements IFtpException{
+public final class FtpFileException extends Exception implements IFtpException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4777141656466558478L;
 
 }
--- src/com/orange/ftpserver/handler/AbstractFtpHandler.java
+++ src/com/orange/ftpserver/handler/AbstractFtpHandler.java
@@ -1,22 +1,28 @@
 package com.orange.ftpserver.handler;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 
 import com.orange.ftpserver.context.DefaultFtpContext;
+import com.orange.ftpserver.context.DefaultFtpResponse;
 import com.orange.ftpserver.context.IFtpSession;
-import com.orange.ftpserver.exception.IFtpException;
+import com.orange.ftpserver.exception.FtpCommandException;
 
 public abstract class AbstractFtpHandler extends SimpleChannelHandler {
 	protected IFtpSession session;
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent cause) throws Exception
 	{ 
-		if(cause instanceof IFtpException){
+		if(cause.getCause() instanceof FtpCommandException){
+			FtpCommandException ftpCommandException = (FtpCommandException)cause.getCause();
 			DefaultFtpContext ftpContext = (DefaultFtpContext)session.getFtpContext();
 			ftpContext.deleteSession(session.getSessionId());
+			DefaultFtpResponse ftpResponse = (DefaultFtpResponse)session.getResponse();
+			ftpResponse.setCode(StringUtils.isNumeric(ftpCommandException.getMessage()) ? Integer.parseInt(ftpCommandException.getMessage()) : 200);
 			session = null;
+			ctx.getChannel().write(ftpResponse.getMessage());
 		}
 	}
 }
