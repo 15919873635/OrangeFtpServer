edd1f3250d252353ac009ce54350ecbebabf4a7f
15919873635 <15919873635@126.com>
2017-07-21 09:28:14 +0800

    12345

--- src/com/orange/ftpserver/user/DefaultUserFactory.java
+++ src/com/orange/ftpserver/user/DefaultUserFactory.java
@@ -4,6 +4,13 @@ public class DefaultUserFactory implements IUserFactory {
 
 	@Override
 	public IFtpUser createUser() {
-		return new BaseUser();
+		BaseUser user = new BaseUser();
+		user.setEnable(USER_ENABLE);
+		user.setHomedirectory(USER_HOMEDIRECTORY);
+		user.setMaxIdleTime(MAX_IDLE_TIME);
+		user.setMaxPerFileSize(MAX_PER_FILE_SIZE);
+		user.setUserName(USER_NAME);
+		user.setUserPassword(USER_PASSWORD);
+		return user;
 	}
 }
--- src/com/orange/ftpserver/user/IUserFactory.java
+++ src/com/orange/ftpserver/user/IUserFactory.java
@@ -1,5 +1,12 @@
 package com.orange.ftpserver.user;
 
 public interface IUserFactory {
+	public final static String USER_HOMEDIRECTORY = "/";
+	public final static String USER_NAME = "admin";
+	public final static String USER_PASSWORD = "admin";
+	public final static long MAX_IDLE_TIME = 0;
+	public final static long MAX_PER_FILE_SIZE = 0;
+	public final static long MAX_USER_SIZE = 0;
+	public final static boolean USER_ENABLE = true;
 	public IFtpUser createUser();
 }
